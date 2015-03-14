57587f43eab944bdf6f985fde78f3da5039dbda4
Too Lee <tpleeto1@mfc.org>
2015-03-14 00:38:29 -0400

    add compute h function

--- run_astar.m
+++ run_astar.m
@@ -1,23 +1,50 @@
 function run_astar
 
-start.x=1;
-start.y=1;
-final.x=3;
-final.y=3;
-make_map(3,3,start,final,0);
+start.r=1;
+start.c=1;
+final.r=3;
+final.c=3;
+obs(10).pos.r = 0;
+map = make_map(3,3,start,final,0)
+
+compute_h(map);
 
 
 function [start,goal,obstacle,clearpath]=load_constants
-start = 0;
-goal = 1;
-obstacle = 2;
-clearpath = 3;
+start = 7;
+goal = 8;
+clearpath = 1;
+obstacle = 0;
 
 
-function make_map(max_row, max_col, start_pos,final_pos,obstacles)
-[START,GOAL,OBS,CLEAR] = load_constants;
+function map = make_map(max_row, max_col, start_pos, final_pos, obstacles)
+[S,G,O,C] = load_constants;
 
+%map = ones(max_row,max_col)*CLEAR;
+%map(start_pos.r, start_pos.c) = START;
 
-world(max_row,max_col) = 0;
+map = [ S, C, C, O;
+        C, O, C, O;
+        C, O, C, O;
+        C, O, C, G;
+        ];
 
-% -1 obstacles
+function compute_h(map)
+[S,G,O,C] = load_constants;
+
+[ROW,COL] = size(map);
+
+nodes(ROW*COL) = struct('r',[],'c',[],'h',[],'g',[],'f',[],'parent',[])
+
+[goal_r,goal_c] = find(map==G)
+
+n = 1;
+for r = 1:ROW
+  for c = 1:COL
+    nodes(n).r = r;
+    nodes(n).c = c;
+    nodes(n).h = sqrt( abs(goal_r-r)^2 + abs(goal_c-c)^2 );
+    nodes(n)
+    n = n+1;
+  end
+end
